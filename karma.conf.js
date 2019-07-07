aacb81d3fce810b019102fb0d4ad8763d564059b
unknown <expert070419@PC17-PC.cjava.local>
2019-07-07 12:22:11 +0200

    second commit

--- karma.conf.js
+++ karma.conf.js
@@ -4,13 +4,15 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'), /* add this line to disable the karma-phantomjslauncher*/
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular/cli/plugins/karma')
+ 
+
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
